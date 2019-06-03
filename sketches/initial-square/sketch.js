db27724a72b73cc009b47c691c86e4804d9094ce
Denis Costa <deniscostadsc@gmail.com>
2019-06-03 09:48:50 -0300

    Improve a few things

--- sketches/initial-square/sketch.js
+++ sketches/initial-square/sketch.js
@@ -1,10 +1,12 @@
 /* eslint no-undef: 0 */
 /* eslint no-unused-vars: 0 */
-const CANVAS_HEIGHT = 600
+
 const CANVAS_WIDTH = 600
+const CANVAS_HEIGHT = 600
+const SQUARE_SIZE = 100
 
 function setup () {
-  createCanvas(600, 600)
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
 }
 
 function randomNumber (start, end) {
@@ -12,5 +14,10 @@ function randomNumber (start, end) {
 }
 
 function draw () {
-  rect(randomNumber(0, 590), randomNumber(0, 590), 100, 100)
+  rect(
+    randomNumber(0, CANVAS_WIDTH),
+    randomNumber(0, CANVAS_HEIGHT),
+    SQUARE_SIZE,
+    SQUARE_SIZE
+  )
 }
