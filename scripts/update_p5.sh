b8a76998479fd2fc0e3f49564cc343f7f48c6ef8
Denis Costa <deniscostadsc@gmail.com>
2019-06-06 12:53:53 -0300

    Refactor update p5 script

--- scripts/update_p5.sh
+++ scripts/update_p5.sh
@@ -2,20 +2,27 @@
 
 set -euo pipefail
 
-LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' \
-    https://github.com/processing/p5.js/releases/latest)
-# shellcheck disable=SC2001
-LATEST_VERSION=$(echo "$LATEST_RELEASE" |\
-    sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+function download_released_file_from_github {
+    user_repo=$1
+    file=$2
+    destiny=$3
 
-cd lib || exit 1
+    latest_release=$(curl -L -s -H 'Accept: application/json' \
+        https://github.com/processing/p5.js/releases/latest)
+    # shellcheck disable=SC2001
+    latest_version=$(echo "$latest_release" |\
+        sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 
-mv p5.min.js old_p5.min.js
-mv p5.sound.min.js old_p5.sound.min.js
-mv p5.dom.min.js old_p5.dom.min.js
+    cd "$destiny" || exit 1
 
-wget -qc "https://github.com/processing/p5.js/releases/download/$LATEST_VERSION/p5.min.js"
-wget -qc "https://github.com/processing/p5.js/releases/download/$LATEST_VERSION/p5.sound.min.js"
-wget -qc "https://github.com/processing/p5.js/releases/download/$LATEST_VERSION/p5.dom.min.js"
+    mv "$file" "old_$file"
 
-rm old_*
+    wget -qc "https://github.com/$user_repo/releases/download/$latest_version/$file"
+
+    rm old_*
+    cd - || exit 1
+}
+
+download_released_file_from_github "processing/p5.js" "p5.dom.min.js" "lib"
+download_released_file_from_github "processing/p5.js" "p5.min.js" "lib"
+download_released_file_from_github "processing/p5.js" "p5.sound.min.js" "lib"
